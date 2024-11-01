f2699d36efaefb9166d884b6656ed19d4373a787
Mehrzut <mhrzd.dev@gmail.com>
2024-11-01 14:32:11 +0330

    dependencies added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:oauth2_client/github_oauth2_client.dart';
+import 'package:oauth2_client/oauth2_client.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
