799635b62a64bb7473b81cb035c65f33dea60029
Mehrzut <mhrzd.dev@gmail.com>
2024-11-02 17:15:36 +0330

    added env file and router

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'dart:developer';
 
+import 'package:code_streak/router.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter/material.dart';
-import 'package:oauth2_client/github_oauth2_client.dart';
-import 'package:oauth2_client/oauth2_client.dart';
 
-void main() {
+void main() async {
+  await _loadEnv();
   runApp(const MainApp());
+}
+
+Future _loadEnv() async {
+  try {
+    await dotenv.load(fileName: "assets/.env");
+  } catch (e) {
+    log('env file not found!');
+  }
 }
 
 class MainApp extends StatefulWidget {
@@ -17,13 +26,14 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp.router(
+      routerConfig: router,
     );
   }
 }
