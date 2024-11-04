a68745854d2411bef5a3e6463cdb6db75fa6646c
Mehrzut <mhrzd.dev@gmail.com>
2024-11-04 21:02:05 +0330

    code refactor

--- lib/features/home/presentation/widgets/contribution_calendar_widget.dart
+++ lib/features/home/presentation/widgets/contribution_calendar_widget.dart
@@ -18,13 +18,17 @@ class ContributionCalendarWidget extends StatefulWidget {
 class _ContributionCalendarWidgetState
     extends State<ContributionCalendarWidget> {
   List<ContributionDayData> allDays = [];
-
+  List<ContributionDayData> currentDays = [];
+  int firstWeekday = 0;
   late DateTime current;
 
   @override
   void initState() {
     allDays = widget.data.contributionCalendar.fold(<ContributionDayData>[],
         (previousValue, element) => [...previousValue, ...element.days]);
+    currentDays = getDaysWithSameMonthAs(current);
+    firstWeekday = currentDays.first.date.weekday % 7;
+
     current = DateTime.now();
     super.initState();
   }
@@ -33,49 +37,35 @@ class _ContributionCalendarWidgetState
   Widget build(BuildContext context) {
     return SizedBox(
       width: MediaQuery.sizeOf(context).width,
-      height: 250,
+      height: 210,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: List.generate(current.monthLengthInWeeks, (weekIndex) {
-          return Expanded(
-            child: Column(
-              children: List.generate(7, (dayIndex) {
-                final index = weekIndex * 7 + dayIndex;
-                final currentDays = getDaysWithSameMonthAs(current);
-                final firstWeekday = currentDays.first.date.weekday % 7;
-                final i = index - firstWeekday;
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(7, (dayIndex) {
+              final index = weekIndex * 7 + dayIndex;
+              final i = index - firstWeekday;
 
-                if (firstWeekday > index) {
-                  // previous month's days
-                  return const Expanded(child: SizedBox());
-                }
-                if (currentDays.length > i) {
-                  // current month's days with data
-                  return Expanded(
-                    child: Container(
-                      margin: const EdgeInsets.all(2),
-                      color: Colors.red,
-                      child: Center(
-                        child: Text(currentDays[i].date.day.toString()),
-                      ),
-                    ),
-                  );
-                }
-                if (i + 1 > current.monthLengthInDays) {
-                  // next month's days
-                  return const Expanded(child: SizedBox());
-                }
-                // current month's days without data
-                return Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(2),
-                    color: Colors.grey,
-                    child: Center(
-                      child: Text((i + 1).toString()),
-                    ),
-                  ),
-                );
-              }),
-            ),
+              if (firstWeekday > index) {
+                // previous month's days
+                return const Expanded(child: SizedBox());
+              }
+              if (currentDays.length > i) {
+                // current month's days with data
+                return _DayItemWidget(data: currentDays[i]);
+              }
+              if (i + 1 > current.monthLengthInDays) {
+                // next month's days
+                return const Expanded(child: SizedBox());
+              }
+              // current month's days without data
+              final date = currentDays.last.date
+                  .add(Duration(days: i - currentDays.length + 1));
+              return _DayItemWidget(
+                data: ContributionDayData(date: date, contributionCount: 0),
+              );
+            }),
           );
         }),
       ),
@@ -90,5 +80,26 @@ class _ContributionCalendarWidgetState
       }
     }
     return temp;
+  }
+}
+
+class _DayItemWidget extends StatelessWidget {
+  const _DayItemWidget({required this.data});
+
+  final ContributionDayData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.square(
+      dimension: 30,
+      child: Container(
+        margin: const EdgeInsets.all(2),
+        decoration: BoxDecoration(
+            color: Colors.grey, borderRadius: BorderRadius.circular(4)),
+        child: Center(
+          child: Text(data.date.day.toString()),
+        ),
+      ),
+    );
   }
 }
