384d58fb7b7af3cbe22455e2939d1d43cb9595e9
Mehrzut <mhrzd.dev@gmail.com>
2025-01-09 15:07:16 +0330

    loading avatar skeletonizer

--- lib/features/home/presentation/widgets/user_avatar.dart
+++ lib/features/home/presentation/widgets/user_avatar.dart
@@ -2,6 +2,7 @@ import 'package:code_streak/core/extensions.dart';
 import 'package:code_streak/features/home/presentation/bloc/user_info_bloc.dart';
 import 'package:code_streak/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 class UserAvatar extends StatelessWidget {
   const UserAvatar({super.key, required this.state, required this.size});
@@ -10,18 +11,24 @@ class UserAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return state.maybeWhen(
-      success: (data) => Container(
-        width: size,
-        height: size,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(size / 2),
-            image: DecorationImage(
-                image: data.avatarUrl.isNotNullOrEmpty
-                    ? NetworkImage(data.avatarUrl ?? '')
-                    : Assets.images.avatar.provider())),
+    return Skeletonizer(
+      enabled: state.maybeWhen(
+        orElse: () => false,
+        loading: () => true,
       ),
-      orElse: () => _defaultAvatar,
+      child: state.maybeWhen(
+        success: (data) => Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(size / 2),
+              image: DecorationImage(
+                  image: data.avatarUrl.isNotNullOrEmpty
+                      ? NetworkImage(data.avatarUrl ?? '')
+                      : Assets.images.avatar.provider())),
+        ),
+        orElse: () => _defaultAvatar,
+      ),
     );
   }
 
